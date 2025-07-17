b82ee98e8577cb896e98108a02570c1698f60343
Fitrasoft Dev <dev.fitrasoft@gmail.com>
2025-07-17 13:01:49 +0700

    20250717.1301 [PreRegist] - Commit missing parts - TokenVerifyResponse.cs in neo.preregist

---
+++ APIs/neo.preregist/Models/TokenVerifyResponse.cs
@@ -0,0 +1,12 @@
+﻿using neo.preregist.Data.Enterprise.Entities;
+using Shared.Common.Models;
+using System.Text.Json.Serialization;
+
+namespace neo.preregist.Models
+{
+    public sealed class TokenVerifyResponse : CommonAPIBodyResponse
+    {
+        [JsonPropertyName("data")]
+        public PreRegist? Data { get; set; }
+    }
+}
