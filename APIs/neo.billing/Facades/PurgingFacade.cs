4fbb29a047ffb5386193ac49ab5ba2348ac44d1a
Fitrasoft Dev <dev.fitrasoft@gmail.com>
2025-08-27 14:22:41 +0700

    20250827.1422 - Stash changes

---
+++ APIs/neo.billing/Facades/PurgingFacade.cs
@@ -0,0 +1,32 @@
+﻿using neo.scheduler.Data.Enterprise;
+using Shared.Entities.Queries.Enterprise;
+
+namespace neo.scheduler.Facades
+{
+    public interface IPurgingFacade
+    {
+        Task<int> PurgeOldInactiveRecordsAsync(CancellationToken ct);
+    }
+
+    public class PurgingFacade
+    {
+        private readonly OtpTokenQueries _otpQueries;
+
+        public PurgingFacade(EnterpriseDbContext edb)
+        {
+            _otpQueries = new OtpTokenQueries(edb);
+        }
+
+        public async Task<int> PurgeOldInactiveRecordsAsync(CancellationToken ct)
+        {
+            int result = 0;
+
+            result += await PurgeOldOtpAsync(ct); // removed used & expired OTPs (and reset AI counter if there are no records)
+
+            return result;
+        }
+
+        private async Task<int> PurgeOldOtpAsync(CancellationToken ct)
+            => await _otpQueries.PurgeUsedExpiryOtpAsync(ct);
+    }
+}
---
+++ APIs/neo.billing/ScheduledProcessor/PurgingTask.cs
@@ -0,0 +1,31 @@
+﻿using neo.scheduler.Data.Enterprise;
+using neo.scheduler.Facades;
+
+namespace neo.scheduler.ScheduledProcessor
+{
+    public class PurgingTask : ScheduledProcessor
+    {
+        protected override TimeSpan InitialDelay => TimeSpan.FromSeconds(30); //delay initial execution by 30 seconds to ease Startup process
+        protected override string Schedule => $"5 * * * *"; //every 5 minute
+
+        public PurgingTask(IServiceScopeFactory serviceScopeFactory, CancellationToken ct) : base(serviceScopeFactory, ct)
+        {
+
+        }
+
+        public async override Task ProcessInScope(IServiceProvider serviceProvider, CancellationToken ct)
+        {
+            try
+            {
+                var dbContext = serviceProvider.GetRequiredService<EnterpriseDbContext>();
+                var purgingFacade = new PurgingFacade(dbContext);
+
+                await purgingFacade.PurgeOldInactiveRecordsAsync(ct);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
